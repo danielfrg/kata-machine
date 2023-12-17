aeacc2b52dbe33fd80b54508a5f1d03434b0e8a2
Daniel Rodriguez <daniel@danielfrg.com>
2023-12-17 13:44:49 -0600

    binary search and two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let dist = Math.floor(Math.sqrt(breaks.length));
 
+    let i = dist;
+    for (; i < breaks.length; i = i + dist) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i = i - dist;
+
+    for (let j = i; j < i + dist; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
