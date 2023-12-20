9cc8b3b58a49b3eef80367940b9fa4005ffb89bc
Daniel Rodriguez <daniel@danielfrg.com>
2023-12-19 22:56:35 -0600

    Linked lists

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,59 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
 
-    
+    public head?: Node<T>;
+    public tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
 
-}
+        const node = { value: item } as Node<T>;
+
+        if (!this.head) {
+            this.head = this.tail = node;
+        } else {
+            this.tail!.next = node;
+            this.tail = node;
+        }
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        // free (not needed in JS)
+        // head.next = undefined;
+        return head.value;
+    }
+
     peek(): T | undefined {
+        return this.head?.value;
+    }
 
-}
+    print(): void {
+        let c = this.head;
+        console.log("!!!", this.head);
+        for (let i = 0; i < this.length; i++) {
+            if (c && c.value) {
+                console.log(c.value);
+            }
+            if (c && c.next) {
+                c = c.next;
+            }
+        }
+    }
 }
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,151 @@
 export default class SinglyLinkedList<T> {
     public length: number;
 
-    
+    public first: Node<T> | undefined;
+    public last: Node<T> | undefined;
 
-    constructor() {
+    constructor() {}
+
+    print(): void {
+        if (this.first == undefined) {
+            return;
+        }
+
+        let curr: Node<T> = this.first;
+        for (let i = 0; i < this.length; ++i) {
+            console.log(curr.value);
+            if (curr.next) {
+                curr = curr.next;
+            }
+        }
+    }
+
+    append(item: T): void {
+        if (this.first == undefined && this.last == undefined) {
+            this.first = new Node(item);
+            this.last = this.first;
+            this.length = 1;
+        } else if (this.last != undefined) {
+            this.last.next = new Node(item);
+            this.last = this.last.next;
+            this.length++;
+        }
     }
 
     prepend(item: T): void {
+        if (this.first == undefined && this.last == undefined) {
+            this.first = new Node(item);
+            this.last = this.first;
+            this.length = 1;
+        } else {
+            const first = new Node(item);
+            first.next = this.first;
+            this.first = first;
+            this.length++;
+        }
+    }
 
-}
-    insertAt(item: T, idx: number): void {
+    insertAt(item: T, idx: number): void {}
 
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
     get(idx: number): T | undefined {
+        if (this.first == undefined) {
+            return undefined;
+        }
 
-}
+        let curr: Node<T> = this.first;
+        for (let i = 0; i < idx; ++i) {
+            if (curr.next) {
+                curr = curr.next;
+            } else {
+                return undefined;
+            }
+        }
+
+        return curr.value;
+    }
+
     removeAt(idx: number): T | undefined {
+        if (this.first == undefined || idx > this.length) {
+            return undefined;
+        }
 
+        if (idx == 0) {
+            const val = this.first.value;
+            this.first = this.first.next;
+            this.length--;
+            return val;
+        }
+
+        let prev: Node<T> = this.first;
+        let curr: Node<T> = this.first.next!;
+        for (let i = 1; i <= idx; ++i) {
+            if (i == idx) {
+                if (idx == this.length) {
+                    prev.next = undefined;
+                    this.last = prev;
+                } else if (prev.next && curr.next) {
+                    prev.next = curr.next;
+                }
+                this.length--;
+                return curr.value;
+            }
+
+            if (curr.next) {
+                prev = prev.next!;
+                curr = curr.next;
+            } else {
+                return undefined;
+            }
+        }
+
+        return undefined;
+    }
+
+    remove(item: T): T | undefined {
+        if (this.first == undefined) {
+            return undefined;
+        }
+
+        if (this.first.value == item) {
+            const val = this.first.value;
+            this.first = this.first.next;
+            this.length--;
+            return val;
+        }
+
+        let prev: Node<T> = this.first;
+        let curr: Node<T> = this.first.next!;
+        for (let i = 1; i < this.length; ++i) {
+            if (curr.value == item) {
+                if (i == this.length - 1) {
+                    prev.next = undefined;
+                    this.last = prev;
+                } else if (prev.next && curr.next) {
+                    prev.next = curr.next;
+                }
+                this.length--;
+                return curr.value;
+            }
+
+            if (curr.next) {
+                prev = prev.next!;
+                curr = curr.next;
+            } else {
+                return undefined;
+            }
+        }
+
+        return curr.value;
+    }
 }
+
+class Node<T> {
+    public next: Node<T> | undefined;
+
+    public value: T;
+
+    constructor(item: T) {
+        this.value = item;
+        this.next = undefined;
+    }
 }
